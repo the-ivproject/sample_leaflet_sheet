876963223ef5e7dd2d52ccac0663ef464de34f2f
the-ivproject <84888011+the-ivproject@users.noreply.github.com>
2021-06-12 00:31:50 +0700

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ZxvU8eGyuN9M8GxTU9acKVJv70iC3px_m3EVFsOHN9g/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1aVIfbSTInOwUOsj4mw7dkbi0pZ69vttlAqBvgXdvss8/edit#gid=164271551';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
-var googleApiKey = 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY';
+var googleApiKey = 'AIzaSyBxy4juu30u8ac7Cd5bsx2dhQeapOVs_i4';
